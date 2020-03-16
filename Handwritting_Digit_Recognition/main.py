b97eb76b85c5257a4bc1feb161aed4348a70d00c
zzj <40758881+Zzj-pcl@users.noreply.github.com>
2020-03-16 20:59:02 +0800

    HandwriteNumberRecog

---
+++ Handwritting_Digit_Recognition/main.py
@@ -0,0 +1,15 @@
+﻿
+
+import sys
+from interface import *
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+#程序运行头 GUI初始化界面
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
